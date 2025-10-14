5c602763f1bbdbc56015799c5b4050837ef3ad5b
anVanish <sosvanish@gmail.com>
2025-10-14 09:35:09 +0700

    feat(user-service): add authentication middlewares

--- services/user-service/src/middlewares/errorHandler.js
+++ services/user-service/src/middlewares/errorHandler.js
@@ -2,6 +2,15 @@ const apiResponse = require("../app/utils/apiResponse");
 
 function errorHandler(err, req, res, next) {
     let statusCode = err.statusCode || 500;
+    if (err.name === "TokenExpiredError") {
+        err.message = "Access token expired";
+        statusCode = 401;
+    }
+    if (err.name === "JsonWebTokenError") {
+        err.message = "Invalid access token";
+        statusCode = 403;
+    }
+
     console.error(err);
     res.status(statusCode).json(
         apiResponse({
