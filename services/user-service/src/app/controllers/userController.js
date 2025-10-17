4684d753107b243f37c0c4ce63b451e252f74d06
anVanish <sosvanish@gmail.com>
2025-10-17 12:11:32 +0700

    feat(user-service/user): add update password function

--- services/user-service/src/app/controllers/userController.js
+++ services/user-service/src/app/controllers/userController.js
@@ -1,8 +1,11 @@
+const { BaseCollection } = require("mongoose");
 const Users = require("../models/Users");
 const apiResponse = require("../utils/apiResponse");
 const httpError = require("../utils/httpError");
 const { safeUser, safeUpdateUser } = require("../utils/sanitizeUser");
+const bcrypt = require("bcryptjs");
 
+//POST /users/me
 exports.profile = async (req, res, next) => {
     try {
         const user = await Users.findOne({ _id: req.user._id });
@@ -16,6 +19,7 @@ exports.profile = async (req, res, next) => {
     }
 };
 
+//PUT /users/me
 exports.updateProfile = async (req, res, next) => {
     try {
         const user = await Users.findById(req.user._id);
@@ -29,10 +33,35 @@ exports.updateProfile = async (req, res, next) => {
 
         res.json(
             apiResponse({
-                message: "update successfully",
+                message: "Profile updated",
                 data: { user: safeUser(updatedUser) },
             })
         );
+    } catch (error) {
+        next(error);
+    }
+};
+
+//PUT /users/me/password
+exports.updatePassword = async (req, res, next) => {
+    try {
+        const { oldPassword, newPassword, confirmedPassword } = req.body;
+        if (!oldPassword || !newPassword || !confirmedPassword)
+            throw httpError("Missing required field", 400);
+        if (newPassword !== confirmedPassword)
+            throw httpError("New password do not match", 400);
+
+        const user = await Users.findById(req.user._id).select("+password");
+        if (!user) throw httpError("User not found", 404);
+
+        const isMatch = await bcrypt.compare(oldPassword, user.password);
+        if (!isMatch) throw httpError("Incorrect old password", 401);
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(newPassword, salt);
+        await user.save();
+
+        res.json(apiResponse({ message: "Password updated" }));
     } catch (error) {
         next(error);
     }
--- services/user-service/src/routes/router/userRouter.js
+++ services/user-service/src/routes/router/userRouter.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const {
     profile,
     updateProfile,
+    updatePassword,
 } = require("../../app/controllers/userController");
 
 router.get("/me", profile);
+router.put("/me/password", updatePassword);
 router.put("/me", updateProfile);
 
 module.exports = router;
