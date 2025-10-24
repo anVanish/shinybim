1fc8e2fe7ffa46476cefbba30cb8cbad047a944a
anVanish <sosvanish@gmail.com>
2025-10-24 07:56:47 +0700

    feat(blog-service): add mongo db connection

--- services/blog-service/src/index.js
+++ services/blog-service/src/index.js
@@ -4,10 +4,12 @@ const dotenv = require("dotenv");
 const { route } = require("./routes");
 const { errorHandler } = require("./middlewares/errorHandler");
 const { notfoundHandler } = require("./middlewares/notfoundHandler");
+const mongodb = require("./config/mongo");
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4002;
+mongodb.connect();
 
 //middlewares
 app.use(morgan("short"));
